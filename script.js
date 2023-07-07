2847dc3c65fe633073e0ab5025c96590b6ab2107
Eazaz Shaikh <135410863+EazazShk@users.noreply.github.com>
2023-07-07 17:42:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,10 +7,9 @@ arr.forEach((button) => {
     button.addEventListener("click", (e) => {
         if (e.target.innerHTML === "=") {
             if (string === "") {
-                alert("Enter a valid expression");
+                alert("Enter a number first");
                 return;
             }
-            // Replace % with /100
             string = string.replace(/%/g, "/100");
             string = eval(string);
             if (isNaN(string)) {
@@ -20,13 +19,23 @@ arr.forEach((button) => {
                 input.value = string;
             }
         } else if (e.target.innerHTML === "AC") {
-            string = "";
-            input.value = string;
+            if (string === "") {
+                alert("Enter number to All Clear");
+            } else {
+                string = "";
+                input.value = string;
+            }
         } else if (e.target.innerHTML === "DEL") {
-            string = string.substring(0, string.length - 1);
-            input.value = string;
+            if (input.value === "") {
+                alert("Enter number to delete");
+            } else {
+                string = string.substring(0, string.length - 1);
+                input.value = string;
+            }
         } else if (e.target.id === "toggleSign") {
-            if (string !== "") {
+            if (input.value === "") {
+                alert("Enter a number first");
+            } else {
                 string = eval(string);
                 string = -string;
                 input.value = string;
@@ -36,13 +45,24 @@ arr.forEach((button) => {
                 alert("Enter a number first");
                 return;
             } else if (isOperator(string[string.length - 1])) {
-                // Replace the previous operator with the new one
                 string = string.slice(0, string.length - 1);
             }
             string += e.target.innerHTML;
             input.value = string;
+        } else if (e.target.innerHTML === ".") {
+            if (string.includes(".")) {
+                // Prevent multiple decimal points
+                return;
+            } else {
+                string += e.target.innerHTML;
+                input.value = string;
+            }
         } else {
-            string += e.target.innerHTML;
+            if (string === "0") {
+                string = e.target.innerHTML;
+            } else if (string.length < 10) {
+                string += e.target.innerHTML;
+            }
             input.value = string;
         }
     });

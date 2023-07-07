cbb29880e1046d746bafc503221df1cf7a3729e0
Eazaz Shaikh <135410863+EazazShk@users.noreply.github.com>
2023-07-07 16:26:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,37 +7,53 @@ arr.forEach((button) => {
     button.addEventListener("click", (e) => {
         if (e.target.innerHTML === "=") {
             string = eval(string);
-            input.value = string;
-            if (string == undefined) {
+            if (isNaN(string)) {
                 alert("Please enter a valid number");
                 input.value = "";
-            }
-        } else if (e.target.innerHTML === "AC") {
-            if (string == "") {
-                alert("Enter number to All Clear");
-            }
-            string = "";
-            input.value = string;
-        } else if (e.target.innerHTML === "DEL") {
-            if (input.value == "") {
-                alert("Enter Number to Delete");
             } else {
                 input.value = string;
             }
-            string = string.substring(0, string.length - 1);
-            input.value = string;
-        } else if (e.target.id === "toggleSign") {
-            if (input.value == "") {
-                alert("Enter a number first");
+        } else if (e.target.innerHTML === "AC") {
+            if (string === "") {
+                alert("Enter number to All Clear");
+            } else {
+                string = "";
+                input.value = string;
             }
-            if (string !== "") {
+        } else if (e.target.innerHTML === "DEL") {
+            if (input.value === "") {
+                alert("Enter Number to Delete");
+            } else {
+                string = string.substring(0, string.length - 1);
+                input.value = string;
+            }
+        } else if (e.target.id === "toggleSign") {
+            if (input.value === "") {
+                alert("Enter a number first");
+            } else {
                 string = eval(string);
                 string = -string;
                 input.value = string;
             }
+        } else if (isOperator(e.target.innerHTML)) {
+            if (isOperator(string[string.length - 1])) {
+                string = string.slice(0, string.length - 1);
+            }
+            string += e.target.innerHTML;
+            input.value = string;
         } else {
             string += e.target.innerHTML;
             input.value = string;
         }
     });
 });
+
+function isOperator(value) {
+    return (
+        value === "+" ||
+        value === "-" ||
+        value === "*" ||
+        value === "/" ||
+        value === "%"
+    );
+}
